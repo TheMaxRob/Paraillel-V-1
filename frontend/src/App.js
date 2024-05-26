941ff220cc25046698ba2ecd9ce022b8c3ca3f4d
Max Roberts <133835313+TheMaxRob@users.noreply.github.com>
2024-05-26 20:08:41 +0100

    Added LessonPlanContext

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,18 +10,34 @@ function App() {
 
   // Function to render the appropriate component based on state
   const renderPage = () => {
-    if (currentPage === 'chat') {
-      return <ChatGPT setCurrentPage={setCurrentPage} />;
-    } else if (currentPage === 'cale') {
-      return <Cale />;
+    switch (currentPage) {
+      case 'chat':
+        return <ChatGPT />;
+      case 'cale':
+        return <Cale />;
+      case 'TheSettings':
+        return <TheSettings />;
+      case 'home':
+        return <Home setCurrentPage={handleNavigation} />;
+      case 'login':
+        return <Login onLogin={handleLogin} />;
+      case 'LoginSuccess':
+        return <LoginSuccess onSubmit={handleSubmit} />;
+      default:
+        return <div>Page not found</div>;
     }
   };
 
   return (
     <LessonPlanContextProvider>
       <div className="App">
-        {renderPage()}
-      </div>
+      {showHome && <Home setCurrentPage={handleNavigation} />} 
+      {renderPage()}
+      {isLoggedIn && (
+        <>
+        </>
+      )}
+    </div>
     </LessonPlanContextProvider>
   );
 }
